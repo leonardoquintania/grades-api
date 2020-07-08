d0263e8a62cbc7e9a0406dc1d27a85967975b68e
lsquintania <lsquintania@quintania-mac.local>
2020-07-08 07:56:25 -0300

    Adicionado cors

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ const app = express();
 //define o dominio de origem para consumo do servico
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: 'http://localhost:8080',
-  })
-);
-
+// app.use(
+//   cors({
+//     origin: 'http://localhost:8080',
+//   })
+// );
+app.use(cors());
 app.use(gradeRouter);
 
 app.get('/', (req, res) => {
